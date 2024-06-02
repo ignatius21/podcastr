ab6ef9fc402fa4077e3576df551207091e62fe25
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-02 17:03:34 -0300

    feat: Update page titles and headings in create-podcast and discover pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@/components/ui/button'
+import PodcastCard from '@/components/PodcastCard'
+import { podcastData } from '@/components/constants'
 import React from 'react'
 
 const Home = () => {
@@ -6,6 +7,17 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <div className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcast</h1>
+        <div className='podcast_grid'>
+          {podcastData.map(({id,title,description,imgURL}) =>(
+            <PodcastCard
+              key={id}
+              title={title}
+              imgURL={imgURL}
+              description={description}
+              podcastId={id}        
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
