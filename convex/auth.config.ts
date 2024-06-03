6c976bb5d67006d90285caf453858e79259adc66
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-03 07:22:20 -0300

    chore: Update npm dependencies and add Convex client provider to root layout

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: 'https://verified-feline-70.clerk.accounts.dev',
+            applicationID: 'convex',
+        }
+    ]
+};
+
+export default authConfig;
